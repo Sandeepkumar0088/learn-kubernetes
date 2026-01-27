a1a74f9dd7fdda2b9788ecb6cad5968f8c59b9d9
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-01-27 17:22:28 +0530

    terraform eks

--- cluster/main.tf
+++ cluster/main.tf
@@ -1,9 +1,17 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
+}
 resource "aws_eks_cluster" "main" {
   name     = var.env
   role_arn = aws_iam_role.cluster.arn
   version  = "1.34"
   vpc_config {
-    subnet_ids = ["subnet-0b41036a19ffb50d9", "subnet-030e60a7f9451e126"]
+    subnet_ids = ["subnet-0b41036a19ffb50d9", "subnet-03cd379790a054a51"]
   }
   access_config {
     authentication_mode = "API_AND_CONFIG_MAP"
@@ -14,7 +22,7 @@ resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "main"
   node_role_arn   = aws_iam_role.node.arn
-  subnet_ids      = ["subnet-0b41036a19ffb50d9", "subnet-030e60a7f9451e126"]
+  subnet_ids      = ["subnet-0b41036a19ffb50d9", "subnet-03cd379790a054a51"]
   instance_types  = ["t3.xlarge"]
 
   scaling_config {
@@ -30,14 +38,14 @@ resource "aws_eks_node_group" "main" {
 
 resource "aws_eks_access_entry" "workstation" {
   cluster_name      = aws_eks_cluster.main.name
-  principal_arn     = "arn:aws:iam::389841108590:role/sample"
+  principal_arn     = "arn:aws:iam::739561048503:role/workstation-role"
   type              = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "workstation" {
   cluster_name  = aws_eks_cluster.main.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = "arn:aws:iam::389841108590:role/sample"
+  principal_arn = "arn:aws:iam::739561048503:role/workstation-role"
 
   access_scope {
     type       = "cluster"
